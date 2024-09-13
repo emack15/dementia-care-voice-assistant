261cea63b454fc592ba442f90208704364237b1a
emilycenteno <emilycenteno@proton.me>
2024-09-13 12:01:38 -0400

    Initial Commit

---
+++ DemCareVoice/DemCareVoiceApp.swift
@@ -0,0 +1,32 @@
+//
+//  DemCareVoiceApp.swift
+//  DemCareVoice
+//
+//  Created by Emily Centeno on 9/13/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DemCareVoiceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ DemCareVoice/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  DemCareVoice
+//
+//  Created by Emily Centeno on 9/13/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
